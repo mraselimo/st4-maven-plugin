ea68cfc19c4a7233b427aef08a3e59eea2eaa299
22374604 <22374604@sun.ac.za>
2020-04-21 20:10:46 +0200

    Add ST4Mojo

---
+++ src/main/java/za/ac/sun/cs/st4/ST4Mojo.java
@@ -0,0 +1,12 @@
+package za.ac.sun.cs.st4;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+public class ST4Mojo extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
