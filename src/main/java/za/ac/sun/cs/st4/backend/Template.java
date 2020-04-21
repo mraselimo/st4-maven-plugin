e269a303f44f409a144a777c866d4ddff605e6a4
22374604 <22374604@sun.ac.za>
2020-04-22 00:12:52 +0200

    template driver complete

--- src/main/java/za/ac/sun/cs/st4/backend/Template.java
+++ src/main/java/za/ac/sun/cs/st4/backend/Template.java
@@ -2,6 +2,7 @@ package za.ac.sun.cs.st4.backend;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 import org.stringtemplate.v4.AutoIndentWriter;
 import org.stringtemplate.v4.ST;
@@ -9,14 +10,26 @@ import org.stringtemplate.v4.STGroup;
 import org.stringtemplate.v4.STGroupFile;
 import org.stringtemplate.v4.misc.ErrorBuffer;
 
+import za.ac.sun.cs.st4.ST4Mojo;
+
 public class Template {
+    static Map<String, Object> properties;
+    public static void setProperties(Map<String, Object> props) {
+        properties = props;
+    }
+    private static Map<String, Object> getProperties() {
+        return properties;
+    }
     public static void generateSource(String fileName) throws IOException {
         STGroup group  = new STGroupFile(""); //TODO
         ST st = group.getInstanceOf("meta");
 
         FileWriter fileWriter = new FileWriter(fileName);
         ErrorBuffer listener = new ErrorBuffer();
-        // aa big TODO here
+
+        Map<String, Object> members = getProperties();
+        st.add("members", members);
+
         st.write(new AutoIndentWriter(fileWriter), listener);
         fileWriter.flush();
         fileWriter.close();
