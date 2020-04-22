9c543c0cfa09b2bf4945e817ac42d6a22bd8f820
22374604 <22374604@sun.ac.za>
2020-04-22 11:37:56 +0200

    functional plugin for st4

--- src/main/java/za/ac/sun/cs/st4/ST4Mojo.java
+++ src/main/java/za/ac/sun/cs/st4/ST4Mojo.java
@@ -8,9 +8,16 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import org.stringtemplate.v4.*;
+import org.stringtemplate.v4.AutoIndentWriter;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+import org.stringtemplate.v4.misc.ErrorBuffer;
+
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 @Mojo(name = "render", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
@@ -25,15 +32,68 @@ public class ST4Mojo extends AbstractMojo {
     @Parameter(property = "outputDirectory",defaultValue = "${project.build.directory}/generated-sources/templates")
     private File outputDirectory;
 
+    /*
+    * template file
+    **/
+    @Parameter(property = "template")
+    private String template;
+    /*
+    * name of the template to process
+    **/
+    @Parameter(property = "target", defaultValue = "")
+    private String target;
+
+    /*
+    *  name of generated source file
+    * */
+    @Parameter(property = "generatedClassName", defaultValue = "")
+    private String generatedClassName;
+
     @Parameter(property = "properties")
     private Map<String, String> properties;
 
     @Parameter(defaultValue = "${project}")
     MavenProject project;
 
+    private void generateSources(String templateFile, String templateName, String generatedFile) throws IOException {
+        STGroup group = new STGroupFile(templateFile);
+        ST st = group.getInstanceOf(templateName);
+
+        FileWriter writer = new FileWriter(generatedFile);
+        ErrorBuffer listener = new ErrorBuffer();
+
+        for(String key : this.getProperties().keySet()) {
+            st.add(key, this.getProperties().get(key));
+        }
+        st.write(new AutoIndentWriter(writer), listener);
+        writer.flush();
+        writer.close();
+    }
+
+    private Map<String, String> getProperties() {
+        return properties;
+    }
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("***  st4-maven-plugin (su edition) ***");
+        getLog().info("*******************************************");
+        getLog().info("       st4-maven-plugin (su edition)       ");
         getLog().info("This is a minimal plugin for stringtemplate");
+        getLog().info("*******************************************");
+
+        if(!outputDirectory.exists())
+            outputDirectory.mkdirs();
+
+        String templateFile = sourceDirectory.getAbsolutePath() + template;
+        String outputFile = outputDirectory.getAbsolutePath() + generatedClassName;
+        try {
+            this.generateSources(templateFile, target, outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /* tell maven where to look for generated sources */
+        if(project != null) {
+            project.addCompileSourceRoot(outputDirectory.getAbsolutePath());
+        }
     }
 }
